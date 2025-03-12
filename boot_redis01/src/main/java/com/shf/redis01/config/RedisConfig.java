acaa366f122f58ee0464ddd12892d16c4cca6c09
xiao yu <yujing_posj@163.com>
2025-03-12 19:55:09 +0800

    Changes

--- boot_redis01/src/main/java/com/shf/redis01/config/RedisConfig.java
+++ boot_redis01/src/main/java/com/shf/redis01/config/RedisConfig.java
@@ -14,7 +14,7 @@ import java.io.Serializable;
 @Configuration
 public class RedisConfig {
     /**
-     * 保证不是序列化后的乱码配置11111111
+     * 保证不是序列化后的乱码配置dddddd
      */
     @Bean
     public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory) {
